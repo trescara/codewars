8a2c23665f2ec8bbf2f65fb09ee5372e98bc964c
Shaun Carter <thereal.shauncarter@gmail.com>
2018-08-12 14:00:17 -0500

    uploading completed bool to string file.

---
+++ ConvertBooltoString.js
@@ -0,0 +1,13 @@
+//This was a test of if else statements. Fairly simple
+//Could also use a case/switch statement
+
+  break;
+
+function booleanToString(b){
+  if (b == true) {
+  return 'true'
+  }
+  else {
+  return 'false'
+  }
+}
